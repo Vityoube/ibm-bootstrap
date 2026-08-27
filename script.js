82f332fa83355c6da6000b032cc60329f93fc945
Viktor Kalashnykov <viktor.kalashnykov@infinitedata.com>
2026-08-27 22:59:49 +0300

    Added handling of expenses displaying

---
+++ script.js
@@ -0,0 +1,97 @@
+document.addEventListener('DOMContentLoaded', function(){
+    $("#monthlyIncome").text(`$${user.income}`)
+    $("#totalExpenses").text(`$${expenseData.totalExpenses}`)
+    $("#balance").text(`$${user.income - expenseData.totalExpenses}`)
+    handleMonthFilter()
+})
+
+function readExpensesByMonthFilter() {
+    const monthYear = $("#monthFilter").val();
+    return expenseData.expenses.filter(expense => {
+        const date = new Date(expense.date);
+        const expenseYear = date.getUTCFullYear();
+        const expenseMonth = (date.getUTCMonth() + 1).toString().padStart(2, "0");
+        return `${expenseYear}-${expenseMonth}` === monthYear;
+    });
+}
+
+function calculateSummaryData(filteredMonthExpenses) {
+    let totalExpensesPerMonth = 0
+    let summaryData = new Map()
+    filteredMonthExpenses.forEach(expense => {
+        totalExpensesPerMonth += expense.amount
+        const categoryName = expense.categoryName
+        const categoryExpense = summaryData.has(categoryName) ? (summaryData.get(categoryName) + expense.amount) : expense.amount
+        summaryData.set(categoryName, categoryExpense)
+    })
+    const result = []
+    for (let [categoryName, amount]  of summaryData.entries()) {
+        const percentage = (amount / totalExpensesPerMonth * 100).toPrecision(3).toString() + "%"
+        result.push({categoryName: categoryName, percentage: percentage})
+    }
+    return result
+}
+
+function handleMonthFilter() {
+    const filteredMonthExpenses = readExpensesByMonthFilter();
+    const expenseSummaryData = calculateSummaryData(filteredMonthExpenses);
+    Promise.allSettled([
+        renderExpenseSummary(expenseSummaryData),
+        renderExpenseData(filteredMonthExpenses)
+    ])
+}
+
+function updateDateFormat(rawDate) {
+    const date = new Date(rawDate);
+    const month = date.toLocaleString("en-us", { month: "short" });
+    const day = date.getDate();
+    const dayOfWeek = date.toLocaleString("en-us", { weekday: "short" });
+    return `${month} ${day}, ${dayOfWeek}`;
+}
+
+async function renderExpenseSummary(expenseSummaryData) {
+    const summaryTableBody = document.querySelector("#expense-summary tbody");
+    summaryTableBody.innerHTML = "";
+    expenseSummaryData.forEach(function (expenseCat) {
+        const row = document.createElement("tr");
+        const categoryCell = document.createElement("td");
+        categoryCell.textContent = expenseCat.categoryName;
+        row.appendChild(categoryCell);
+        const percentageCell = document.createElement("td");
+        percentageCell.textContent = expenseCat.percentage;
+        row.appendChild(percentageCell);
+        summaryTableBody.appendChild(row);
+    });
+}
+
+async function renderExpenseData(expenses) {
+    const tableBody = document.querySelector("#expense-details tbody");
+    tableBody.innerHTML = "";
+    expenses.forEach(function (expense) {
+        const row = document.createElement("tr");
+        /**Date column**/
+        const dateCell = document.createElement("td");
+        dateCell.textContent = updateDateFormat(expense.date);
+        dateCell.className = "text-nowrap";
+        row.appendChild(dateCell);
+        /**Description column**/
+        const descriptionCell = document.createElement("td");
+        descriptionCell.textContent = expense.description;
+        row.appendChild(descriptionCell);
+        /**Amount column**/
+        const amountCell = document.createElement("td");
+        amountCell.textContent = "$" + expense.amount;
+        row.appendChild(amountCell);
+        /**Edit and Delete icons**/
+        const controlCell = document.createElement("td");
+        controlCell.innerHTML =
+            '<a href="#" class="me-2 edit expButton" data-mode="edit"' +
+            ' data-expense="' + JSON.stringify(expense) + '">' +
+            '<img src="./images/edit.png" alt="Edit" class="icon"/></a>' +
+            '<a href="#" class="delete" data-bs-toggle="modal"' +
+            ' data-bs-target="#deleteModal"><img src="./images/delete.png" alt="Delete" class="icon"/></a>';
+        row.appendChild(controlCell);
+        tableBody.appendChild(row);
+    });
+
+}
